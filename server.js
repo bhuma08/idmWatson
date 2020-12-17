7d7c4d9617a86a6da0789cec246fbc598ff7606d
bhuma08 <62709043+bhuma08@users.noreply.github.com>
2020-12-17 11:17:55 +0000

    make it better

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ const flats = [
 
 server.get('/', (req, res) => res.send({flats}));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Express departing now from http://localhost:${port}`))
