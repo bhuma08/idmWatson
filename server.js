a5cb80e87b15f79d6178dfa0c53d8e5575f87fd6
bhuma08 <62709043+bhuma08@users.noreply.github.com>
2020-12-17 11:11:27 +0000

    flats

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+const cors = require('cors');
+
+server.use(cors());
+
+const flats = [
+    {name: 'St Johns Road, Clarkenwell',
+    price: '£500,000',
+    description:'This is a 2 bedroom flat in the heart of city of London.' }, 
+
+    {name: 'Dance Square, London',
+    price: '£450,000',
+    description:'This is a 1 bedroom flat with a balcony.'}
+];
+
+server.get('/', (req, res) => res.send({flats}));
+
+const port = 3000;
+server.listen(port, () => console.log(`Express departing now from http://localhost:${port}`))
